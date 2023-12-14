7a1ec5a0371b2d3c542bb1adc427705752fdcd69
Elbarto <oyjddkvj@gmail.com>
2023-12-14 11:33:34 +0500

    hive step5 - save/retirieve data

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:flutter_hive/presentation/home_page/home_page.dart';
 import 'package:flutter_hive/presentation/home_page/model/boxes.dart';
 import 'package:flutter_hive/presentation/home_page/model/person.dart';
-import 'package:flutter_hive/presentation/widgets/textfield_decoration.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   await Hive.initFlutter();
   Hive.registerAdapter(PersonAdapter());
   boxPersons = await Hive.openBox<Person>('personBox');
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 0, 45, 111),
-          title: const Text('Flutter Hive'),
-        ),
-        backgroundColor: const Color.fromARGB(255, 0, 45, 111),
-        body: Column(
-          children: [
-            const SizedBox(height: 10),
-            Expanded(
-              child: Container(
-                margin: const EdgeInsets.all(10),
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: textfielDecoration('Name'),
-                    ),
-                    const SizedBox(height: 10),
-                    TextFormField(
-                      decoration: textfielDecoration('Age'),
-                    ),
-                    const SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: const Text('Add '),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 2,
-              child: Container(
-                margin: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(12),
-                ),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Delete All '),
-            ),
-            const SizedBox(height: 40),
-          ],
-        ),
-      ),
-    );
-  }
+  runApp(const HomePage());
 }
---
+++ lib/presentation/home_page/home_page.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hive/presentation/home_page/model/boxes.dart';
+import 'package:flutter_hive/presentation/home_page/model/person.dart';
+import 'package:flutter_hive/presentation/widgets/textfield_decoration.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController ageController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 0, 45, 111),
+          title: const Text('Flutter Hive'),
+        ),
+        backgroundColor: const Color.fromARGB(255, 0, 45, 111),
+        body: Column(
+          children: [
+            const SizedBox(height: 10),
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.all(10),
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: nameController,
+                      textInputAction: TextInputAction.next,
+                      decoration: textfielDecoration('Name'),
+                    ),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      controller: ageController,
+                      decoration: textfielDecoration('Age'),
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          boxPersons.put(
+                            'key_${nameController.text}',
+                            Person(
+                              name: nameController.text,
+                              age: int.parse(ageController.text),
+                            ),
+                          );
+                          nameController.text = '';
+                          ageController.text = '';
+                        });
+                      },
+                      child: const Text('Add '),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                margin: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Column(
+                  children: [
+                    ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: boxPersons.length,
+                      itemBuilder: (context, index) {
+                        Person person = boxPersons.getAt(index);
+
+                        return ListTile(
+                          title: const Text('Name'),
+                          subtitle: Text(person.name),
+                          trailing: Text('Age: ${person.age}'),
+                          leading: IconButton(
+                            onPressed: () {
+                              setState(() {
+                                boxPersons.deleteAt(index);
+                              });
+                            },
+                            icon: const Icon(Icons.remove),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  boxPersons.clear();
+                });
+              },
+              child: const Text('Delete All '),
+            ),
+            const SizedBox(height: 40),
+          ],
+        ),
+      ),
+    );
+  }
+}
